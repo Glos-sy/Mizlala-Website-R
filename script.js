5cf1efa5c9ad46d7a9225e62e07034147ad63bc1
Ayo <baniremubarakah@gmail.com>
2025-01-23 22:53:59 +0100

    webpage completed with full respnsiveness and image slider

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 "use strict";
 
-function showSlides() {
-  const slides = document.getElementsByClassName("mySlides");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function autoSlide() {
+  nextSlide(1);
+}
+
+setInterval(autoSlide, 3000);
+
+function nextSlide(n) {
+  showSlides((slideIndex = slideIndex + n));
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("img-slide");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  } else {
+    slideIndex = n;
+  }
+
+  Array.from(slides).forEach((slide) => {
+    slide.style.display = "none";
+  });
+
+  // UPDATED STYLING
+  slides[slideIndex - 1].style.display = "block";
 }
